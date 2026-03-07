dbf484239b5bcca475b7d80e97b89cd326403ccc
Toxic <gyaneshp606@gmail.com>
2026-03-07 17:46:23 +0530

    Iam-Toxicc

--- ShrutiMusic/platforms/Youtube.py
+++ ShrutiMusic/platforms/Youtube.py
@@ -10,8 +10,8 @@ from typing import Union
 from pyrogram.enums import MessageEntityType
 from pyrogram.types import Message
 from py_yt import VideosSearch, Playlist
-from SONALI_MUSIC.utils.database import is_on_off
-from SONALI_MUSIC.utils.formatters import time_to_seconds
+from ShrutiMusic.utils.database import is_on_off
+from ShrutiMusic.utils.formatters import time_to_seconds
 
 from config import API_URL, VIDEO_API_URL, API_KEY
 
